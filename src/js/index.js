6071730782f2105b8f2256462d3dc63a1c00df74
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-10-08 20:53:13 +0700

    added, fixed and created styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import "../pages/index.css";
+
+const headerMenu = document.querySelector(".header__menu-mobile");
+const openMenu = document.querySelector(".header__menu-logo");
+const closeMenu = document.querySelector(".header__list-close");
+
+function actionWithMenu() {
+  headerMenu.classList.toggle("header__menu-mobile_is-active");
+}
+
+openMenu.addEventListener("click", actionWithMenu);
+closeMenu.addEventListener("click", actionWithMenu);
